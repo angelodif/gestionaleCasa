5affdf6bbbed128fbb47f53f5af6c1603b6db43c
EE87337 <angelo.di.florio@avanade.com>
2026-04-22 15:50:19 +0200

    Delete Register possibility

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./core/auth/login/login.component').then(m => m.LoginComponent)
   },
-  {
-    path: 'register',
-    loadComponent: () => import('./core/auth/register/register.component').then(m => m.RegisterComponent)
-  },
+  //{
+  //  path: 'register',
+  //  loadComponent: () => import('./core/auth/register/register.component').then(m => m.RegisterComponent)
+  //},
   {
     path: 'public-shifts',
     loadComponent: () => import('./features/public-shifts/public-shifts.component').then(m => m.PublicShiftsComponent)
