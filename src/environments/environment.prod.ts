fcca18a639eaf9d50918f85419dfb26e509fc841
EE87337 <angelo.diflorio@libero.it>
2026-05-07 14:56:04 +0200

    fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: "gestionalecasaadf",
+    appId: "1:314406332741:web:6de1dbb1982d43f6b4f62c",
+    storageBucket: "gestionalecasaadf.firebasestorage.app",
+    apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
+    authDomain: "gestionalecasaadf.firebaseapp.com",
+    messagingSenderId: "314406332741",
+    measurementId: "G-TDKWSC14E4"
+  },
+  funnyStation: {
+    apiKey: "FUNNY_STATION_API_KEY_PLACEHOLDER",
+    authDomain: "funny-station-63149.firebaseapp.com",
+    projectId: "funny-station-63149",
+    storageBucket: "funny-station-63149.firebasestorage.app",
+    messagingSenderId: "427747054261",
+    appId: "1:427747054261:web:e523d5bce72470109764fc"
+  }
+};
