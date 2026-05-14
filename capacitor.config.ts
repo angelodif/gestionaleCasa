c4dcf3e047cae508fc3a3fb826efbc2acdb209be
EE87337 <angelo.diflorio@libero.it>
2026-05-14 10:54:15 +0200

    feat: initialize Android project structure and configuration for Capacitor integration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.angelo.gestionalecasa',
+  appName: 'gestionale-casa',
+  webDir: 'dist/gestionale-casa/browser'
+};
+
+export default config;
